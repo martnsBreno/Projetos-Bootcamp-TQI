ca3269cd109ce152fb73293001b4d0e30b1ae20f
Breno Martins de Oliveira <brenojpmartins2002@gmail.com>
2022-06-07 20:04:05 -0300

    patricando sintaxe e validações

---
+++ Javascript/Sintaxe&Operadores/script.js
@@ -0,0 +1,46 @@
+function comparaNumeros(num1, num2) {
+
+    //Validações
+
+    if (!num1 || !num2) return 'Os números precisam ser maiores que 0';
+    if (typeof(num1) != 'number') return 'Você precisa digitar um número';
+    if (typeof(num2) != 'number') return 'Você precisa digitar um número';
+
+	const primeiraFrase = criaPrimeiraFrase(num1, num2);
+	const segundaFrase = criaSegundaFrase(num1, num2);
+
+	return `${primeiraFrase} ${segundaFrase}`;
+}
+
+//Função para criar a primeira parte do output
+
+function criaPrimeiraFrase(num1, num2) {
+	let primeiraFrase = `Os números ${num1} e ${num2}`;
+	let simNao = 'não';
+
+	if (num1 === num2) {
+		simNao = '';
+	}
+
+	return `${primeiraFrase} ${simNao} são iguais.`;
+}
+
+//Função para criar a segunda parte do output
+
+function criaSegundaFrase(num1, num2) {
+	const soma = num1 + num2;
+	let comparaDez = 'menor';
+	let comparaVinte = 'menor';
+
+	if (soma > 10) {
+		comparaDez = 'maior';
+	}
+
+	if (soma > 20) {
+		comparaVinte = 'maior';
+	}
+
+	return `Sua soma é ${soma}, que é ${comparaDez} do que 10 e ${comparaVinte} do que 20.`;
+}
+
+console.log(comparaNumeros(10, 10));
